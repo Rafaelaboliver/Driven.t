89a7969e778067a0538981a7c8591c9921925a6b
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-04-27 13:00:59 -0300

    fix: POST /booking route

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from 'express';
 import httpStatus from 'http-status';
 import { Room } from '@prisma/client';
 import { AuthenticatedRequest } from '@/middlewares';
-import bookingsService from '@/services/bookings-service';
+import bookingsService from '@/services/booking-service';
 
 export async function createBooking(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
